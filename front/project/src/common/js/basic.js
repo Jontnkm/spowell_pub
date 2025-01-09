8622936bc295936e0e6f73243f2feee8e0d50fb5
mypark0727 <pugas0727@gmail.com>
2025-01-09 09:09:13 +0900

    2025.01.09 캘린더 스크롤 기능추가

--- front/project/src/common/js/basic.js
+++ front/project/src/common/js/basic.js
@@ -513,8 +513,41 @@ document.addEventListener("DOMContentLoaded", function () {
             })
 
             box[idx].style.display = "block";
+
+            // 클릭후 발동
+            handleCalendarHeight();
         })
     })
+
+    // 메인 & 복지캘린더 가로스크롤 너비 계산
+    handleCalendarHeight();
+    function handleCalendarHeight(){
+        let calendarDom = document.querySelector('.calendarInner.pcType');
+        if (calendarDom) {
+            let _targetParent = calendarDom.querySelectorAll('.tabCont > div');
+            _targetParent.forEach((tabDom)=>{
+                let _target = tabDom.querySelectorAll('ul')[2];
+                let _fakeBox = tabDom.querySelector('.fakeBox');
+
+                // _target의 height를 계산
+                let targetHeight = _target.scrollHeight;
+                console.log('Target Height:', targetHeight);
+    
+                // 조건에 따라 클래스 추가
+                if (targetHeight > 454) {
+                    _fakeBox.classList.add('addScroll');
+                    console.log('addScroll 추가');
+                    console.log(_target);
+                    console.log(_fakeBox);
+                } else {
+                    _fakeBox.classList.remove('addScroll');
+                    console.log('addScroll 삭제');
+                    console.log(_target);
+                    console.log(_fakeBox);
+                }
+            })
+        }
+    }
     
     //푸터 협력사 슬라이드
     var affSlide = new Swiper(".affSlide", {
